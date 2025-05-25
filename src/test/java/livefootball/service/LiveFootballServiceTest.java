99c719141c1cb2efa93aa3c97741231c8c7dcc04
Radosław Lichtarski <lichtarski.radek@gmail.com>
2025-05-25 22:49:55 +0200

    add the first test for testing if displaying the summary shows no past games

--- src/test/java/livefootball/service/LiveFootballServiceTest.java
+++ src/test/java/livefootball/service/LiveFootballServiceTest.java
@@ -22,6 +22,18 @@ class LiveFootballServiceTest {
     }
 
     @Test
+    public void user_displays_the_games_summary_and_there_are_no_past_matches() {
+        // given
+        final LiveFootballService liveFootballService = new LiveFootballService();
+
+        // when
+        final List<Game> gamesSummary = liveFootballService.getGamesSummary();
+
+        // then
+        assertEquals(gamesSummary.size(), 0);
+    }
+
+    @Test
     public void user_adds_a_game_and_it_adds_this_game_to_games_live_scoreboard_with_0_to_0_score() {
         // given
 
