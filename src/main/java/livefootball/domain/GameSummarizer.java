18b47405a9af3a84a9dfc514be25ac92a9188d30
Radosław Lichtarski <lichtarski.radek@gmail.com>
2025-05-29 21:39:48 +0200

    change putting games into the summary

--- src/main/java/livefootball/domain/GameSummarizer.java
+++ src/main/java/livefootball/domain/GameSummarizer.java
@@ -6,18 +6,21 @@ import java.util.List;
 
 class GameSummarizer {
 
-    private final List<Game> summary = new ArrayList<>();
+    private static final Comparator<Game> TOTAL_DESC =
+            Comparator.comparingInt(Game::getOverallScore).reversed();
 
-    List<Game> getSummary() {
-        return summary.stream()
-                .sorted(Comparator
-                        .comparingInt(Game::getOverallScore)
-                        .reversed())
-                .toList();
-    }
+    private final List<Game> summary = new ArrayList<>();
 
     void add(Game game) {
         summary.add(0, game);
+        summary.sort(
+                (g1, g2) -> Integer.compare(
+                        g2.getOverallScore(),
+                        g1.getOverallScore()));
+    }
+
+    List<Game> getSummary() {
+        return List.copyOf(summary);
     }
 
     String getSummaryGamesInfoAsString() {
